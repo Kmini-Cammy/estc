f73c2104b16e9bf6d79ac1c572e13adfbb16f22f
Thaon-Chenimenil-ESTC <45682280+Thaon-Chenimenil-ESTC@users.noreply.github.com>
2026-01-09 14:16:55 +0100

    Update Joueurs.vue

--- src/components/Joueurs.vue
+++ src/components/Joueurs.vue
@@ -3,8 +3,30 @@
     <Card>
         <template #title>Joueurs</template>
         <template #content>
-            <small>powered by <a target="_blank" href="https://www.pingpocket.fr/" >www.pingpocket.fr</a></small>
-            <iframe frameborder="1" name="pingpocket" width="100%" height="800" scrolling="auto" src="https://www.pingpocket.fr/app/fftt/clubs/06880145/licencies?SORT=ALPHABETIC" ><p>iframe non supportée</p></iframe>
+          <Accordion value="0">
+    <AccordionPanel value="0">
+        <AccordionHeader>Header I</AccordionHeader>
+        <AccordionContent>
+            <p class="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </AccordionContent>
+    </AccordionPanel>
+    <AccordionPanel value="1">
+        <AccordionHeader>Header II</AccordionHeader>
+        <AccordionContent>
+            <p class="m-0">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+                ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+            </p>
+        </AccordionContent>
+    </AccordionPanel>
+    <AccordionPanel value="2">
+    </AccordionPanel>
+</Accordion>
+
+Dynamic
         </template>
     </Card>
 </template>
